fe1d304a3cbb189e5fb24a56d5cff58ec68dfc2a
eediii <67259887+eediii@users.noreply.github.com>
2025-01-16 00:27:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-const cheerio = require("cheerio");
-const express = require("express");
-const fetch = require("node-fetch");
-const slugify = require("slugify");
-const cors = require("cors");
+let cheerio = require("cheerio");
+let express = require("express");
+let fetch = require("node-fetch");
+let slugify = require("slugify");
+let cors = require("cors");
 
 API_URI_1 = "https://www.hurriyet.com.tr/mahmure/astroloji/{0}-burcu/" //burada günlük yorumları
 API_URI_2 = "https://www.hurriyet.com.tr/mahmure/astroloji/{0}-burcu-{1}-yorum/" // burada get değerine göre burcun haftalık,aylık,yıllık yorumunu alacağız
 API_URI_3 = "https://www.hurriyet.com.tr/mahmure/astroloji/burclar/{0}-burcu/{1}"
 // gelecek degerler => AŞK,KARİYER,OLUMLU YONLER,SAĞLIK,STİL,ÜNLÜLER,DİYET,ZIT BURÇLARI,EĞLENCE HAYATİ, MAKYAJ, SAÇ STİLİ, ŞİFALI BİTKİLER, FİLM ÖNERİLERİ, ÇOCUKLUĞU, KADINI, ERKEĞİ
 
-const app = express();
+let app = express();
 app.use(cors({ origin: 'https://4c5ca1-54.myshopify.com' }));
 
 //Gunluk /get/burc
 
 app.get("/get/:burc", async (req, res) => {
-    const burc = req.params.burc;
-    const datas = [];
+    let burc = req.params.burc;
+    let datas = [];
 
     await fetch(API_URI_1.replace('{0}', slugify(burc)))
         .then(response => response.text())
         .then(body => {
-            const $ = cheerio.load(body);
-            const MottoText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(1)').text();
-            const motto = MottoText.split(":  ")[1];
-            const GezegenText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(2)').text();
-            const gezegen = GezegenText.split(":  ")[1];
-            const ElementText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(3)').text();
-            const element = ElementText.split(":  ")[1];
-            const yorum = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-2.col-lg-8.col-md-12 > div > div.horoscope-detail-tab > div.horoscope-detail-content > div > p').filter((_, el) => !$(el).text().includes("AYGÜL AYDIN")).map((_, el) => $(el).text()).get().join("\n");
-            
+            let $ = cheerio.load(body);
+            let MottoText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(1)').text();
+            let motto = MottoText.split(":  ")[1];
+            let GezegenText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(2)').text();
+            let gezegen = GezegenText.split(":  ")[1];
+            let ElementText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(3)').text();
+            let element = ElementText.split(":  ")[1];
+            let yorum = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-2.col-lg-8.col-md-12 > div > div.horoscope-detail-tab > div.horoscope-detail-content > div > p')
+            .filter((_, el) => {
+                let text = $(el).text().toLowerCase();
+                return !text.includes("aygül aydın");
+            }).map((_, el) => $(el).text()).get().join("\n");
             if (motto && gezegen && element && yorum) {
                 datas.push({
                     Burc: burc.charAt(0).toUpperCase() + burc.slice(1),
@@ -55,21 +58,25 @@ app.get("/get/:burc", async (req, res) => {
 //gunluk yorum için herhangi bir değere gerek yoktur => /get/burc yeterlidir
 
 app.get("/get/:burc/:zaman", async (req, res) => {
-    const burc = req.params.burc;
-    const zaman = req.params.zaman;
-    const datas = [];
+    let burc = req.params.burc;
+    let zaman = req.params.zaman;
+    let datas = [];
 
     await fetch(API_URI_2.replace('{0}', slugify(burc)).replace('{1}', slugify(zaman)))
         .then(response => response.text())
         .then(body => {
-            const $ = cheerio.load(body);
-            const MottoText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(1)').text();
-            const motto = MottoText.split(":  ")[1];
-            const GezegenText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(2)').text();
-            const gezegen = GezegenText.split(":  ")[1];
-            const ElementText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(3)').text();
-            const element = ElementText.split(":  ")[1];
-            const yorum = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-2.col-lg-8.col-md-12 > div > div.horoscope-detail-tab > div.horoscope-detail-content > div > p').filter((_, el) => !$(el).text().includes("AYGÜL AYDIN")).map((_, el) => $(el).text()).get().join("\n");
+            let $ = cheerio.load(body);
+            let MottoText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(1)').text();
+            let motto = MottoText.split(":  ")[1];
+            let GezegenText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(2)').text();
+            let gezegen = GezegenText.split(":  ")[1];
+            let ElementText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(3)').text();
+            let element = ElementText.split(":  ")[1];
+            let yorum = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-2.col-lg-8.col-md-12 > div > div.horoscope-detail-tab > div.horoscope-detail-content > div > p')
+            .filter((_, el) => {
+                let text = $(el).text().toLowerCase();
+                return !text.includes("aygül aydın");
+            }).map((_, el) => $(el).text()).get().join("\n");
             
             if (motto && gezegen && element && yorum) {
                 datas.push({
@@ -98,16 +105,26 @@ app.get("/get/:burc/:zaman", async (req, res) => {
 // gelecek degerler => AŞK,KARİYER,OLUMLU YONLER,SAĞLIK,STİL,ÜNLÜLER,DİYET,ZIT BURÇLARI,EĞLENCE HAYATİ, MAKYAJ, SAÇ STİLİ, ŞİFALI BİTKİLERi, FİLM ÖNERİLERİ, ÇOCUKLUĞU, KADINI, ERKEĞİ
 
 app.get("/gets/:burc/:ozellik", async (req, res) => {
-    const burc = req.params.burc;
-    const ozellik = req.params.ozellik;
-    const datas = [];
+    let burc = req.params.burc;
+    let ozellik = req.params.ozellik;
+    let datas = [];
 
     await fetch(API_URI_3.replace('{0}', slugify(burc)).replace('{1}', slugify(ozellik)))
         .then(response => response.text())
         .then(body => {
-            const $ = cheerio.load(body);
-            const baslik = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content__inf > h2').text();
-            const yorum = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > p').text();
+            let $ = cheerio.load(body);
+            let baslik = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content__inf > h2').text();
+            let yorum;
+            if (ozellik === "unluler") {
+                yorum = $('body > div.page-wrapper.news-detail-page.Article.o-visible > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > ul > li').map((_, el) => $(el).text()).get().join("\n");
+            } else {
+                yorum = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-2.col-lg-8.col-md-12 > div > div.horoscope-detail-tab > div.horoscope-detail-content > div > p')
+                .filter((_, el) => {
+                    let text = $(el).text().toLowerCase();
+                    return !text.includes("aygül aydın");
+                }).map((_, el) => $(el).text()).get().join("\n");
+            }
+            
             if (baslik && yorum) {
                 datas.push({
                     Burc: burc.charAt(0).toUpperCase() + burc.slice(1),
@@ -128,7 +145,7 @@ app.get("/gets/:burc/:ozellik", async (req, res) => {
     }
 });
 
-const port = process.env.PORT || 5000;
+let port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log("Sunucu ayakta")
 })
