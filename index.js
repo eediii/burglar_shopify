3136013e15fc3417d12caffb7fbee431783946e3
eediii <67259887+eediii@users.noreply.github.com>
2025-01-15 22:40:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get("/get/:burc/:zaman", async (req, res) => {
             const gezegen = GezegenText.split(":  ")[1];
             const ElementText = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-1.col-12 > div.row.mb20 > div > div > div.horoscope-menu-detail > ul > li:nth-child(3)').text();
             const element = ElementText.split(":  ")[1];
-            const yorum = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > p').text();
+            const yorum = $('body > div.main-wrapper > div:nth-child(2) > div > div > div.region-type-2.col-lg-8.col-md-12 > div > div.horoscope-detail-tab > div.horoscope-detail-content > div').text();
             
             if (motto && gezegen && element && yorum) {
                 datas.push({
