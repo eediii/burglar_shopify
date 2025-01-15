867ce7c60d5895887f5ab90e0ac24606832e08cd
eediii <67259887+eediii@users.noreply.github.com>
2025-01-16 02:39:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ app.get("/gets/:burc/:ozellik", async (req, res) => {
                 yorum = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > p').text();
             }
             
-            if (baslik && yorum) {
+            if (baslik) {
                 datas.push({
                     Burc: burc.charAt(0).toUpperCase() + burc.slice(1),
                     Ozellik: ozellik.charAt(0).toUpperCase() + ozellik.slice(1),
