a947f5e094bccee099e073e0f33fa42dcdbbd8c9
eediii <67259887+eediii@users.noreply.github.com>
2025-01-16 01:01:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get("/gets/:burc/:ozellik", async (req, res) => {
                 .get()
                 .join("\n");
             } else {
-                yorum = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > p').join("\n");
+                yorum = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > p').text();
             }
             
             if (baslik && yorum) {
