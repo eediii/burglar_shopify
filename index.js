5b55d93c2d2204b3f2deed71e20832919beee71f
eediii <67259887+eediii@users.noreply.github.com>
2025-01-16 03:38:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -109,11 +109,14 @@ app.get("/gets/:burc/:ozellik", async (req, res) => {
             let baslik = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content__inf > h2').text();
             let yorum;
             if (ozellik === "unluler") {
-                yorum = {};
+                let guidelines = [];
                 $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > ul > li').each((idx, el) => {
                     const guideline = $(el).text();
-                    yorum.push(guideline);
+                    guidelines.push(guideline);
                 });
+
+                // Sonuçları birleştir
+                yorum = guidelines.join("\n");
             } else {
                 yorum = $('body > div.page-wrapper.news-detail-page.Article > section.news-detail-content > div.container > div:nth-child(5) > div.col-xl-17.col-lg-16.news-left-content > div.news-content.readingTime > p').text();
             }
